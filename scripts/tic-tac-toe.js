cbe434c3e1678795c293fb4679ed0008d2e701a1
Grant Daigle <grantdaigle@yahoo.com>
2021-03-28 18:20:11 -0500

    modified tic-tac-toe.html and tic-tac-toe.js

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -3,8 +3,11 @@ var currentPlayer = 'X';
 var totalMoves = 1;
 var gameTied = false;
 var buttonClick = false;
+document.getElementById('sButton').addEventListener("click", startButtonClicked);
+
+
 function placeMarker(spotID){
-    if(buttonClick === true){
+    if(buttonClick === true){   
     if(!gameOver){
     var spot = document.getElementById(spotID);
     if(spot.innerHTML !== 'X' && spot.innerHTML !== 'O'){
@@ -29,6 +32,10 @@ function placeMarker(spotID){
     updateGameStatus();
     }
     }
+    else if(!gameOver){
+        alert('Press Start to Play');
+    }
+
     }
 
 function currentPlayerWon(){
@@ -58,7 +65,6 @@ function currentPlayerWon(){
             buttonClick = false;
             updateGameStatus();
         }
-
     }
 }
 
@@ -67,68 +73,19 @@ function updateGameStatus(){
     if(gameTied){
         statusBoard.innerHTML = 'It is a Tie';
         buttonClick = false;
-        showStatus();
-
+      
     }
     else if(gameOver){
         statusBoard.innerHTML = 'Player ' + currentPlayer + ' Won';
         buttonClick = false;
-        showStatus();
+     
     }
     else{
         statusBoard.innerHTML = 'It is Player ' + currentPlayer + '\'s turn';
     }
 }
-function showStatus(){
-    var showStatusBoard = document.getElementById("status");
-    var showButton = document.getElementById("sButton");
-    if(gameOver){
-        showButton.style.display="block";
-        showButton.innerHTML = 'Replay';
-        buttonClick = false;
-        }
-        if(showButton.innerHTML === 'Replay'){
-            buttonClick = false;
-        }
-        if(gameOver && showButton.innerHTML === 'Replay' && showButton.onclick){
-            alert('Has the Button been Clicked? ' + buttonClick);
-            reset();
-        }
-        
-        
-}
-function reset(){
-            gameOver = false;
-            gameTied = false;
-            currentPlayer = 'X';
-            var s1 = document.getElementById('spot1');
-            var s2 = document.getElementById('spot2');
-            var s3 = document.getElementById('spot3');
-            var s4 = document.getElementById('spot4');
-            var s5 = document.getElementById('spot5');
-            var s6 = document.getElementById('spot6');
-            var s7 = document.getElementById('spot7');
-            var s8 = document.getElementById('spot8');
-            var s9 = document.getElementById('spot9');
-
-            s1.style.color = '#61429d';
-            s2.style.color = '#61429d';
-            s3.style.color = '#61429d';
-            s4.style.color = '#61429d';
-            s5.style.color = '#61429d';
-            s6.style.color = '#61429d';
-            s7.style.color = '#61429d';
-            s8.style.color = '#61429d';
-            s9.style.color = '#61429d';
-            
-            s1.innerHTML = 1;
-            s2.innerHTML = 2;
-            s3.innerHTML = 3;
-            s4.innerHTML = 4;
-            s5.innerHTML = 5;
-            s6.innerHTML = 6;
-            s7.innerHTML = 7;
-            s8.innerHTML = 8;
-            s9.innerHTML = 9;
-
+function startButtonClicked(){
+    document.getElementById('sButton').style.display='none';
+    document.getElementById('status').style.display='block'
+    buttonClick = true;
 }
